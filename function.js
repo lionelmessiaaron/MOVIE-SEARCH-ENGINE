f8f9e4c395de7ed333bd738b32a16100b0b47073
Aaron Samuel Mathew <103445014+lionelmessiaaron@users.noreply.github.com>
2023-02-27 22:22:15 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,6 @@
+const searchBtn = document.querySelector(".search i");
+const searchInput = document.querySelector(".search input");
+
+searchBtn.addEventListener("click", function() {
+    searchInput.focus();
+});
